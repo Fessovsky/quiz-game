c5f2f9eaf323ba5c27bc242fcb8f923933f32b80
fessovsky <angrycustomer@yandex.ru>
2022-06-22 20:28:58 +0300

    add render for questions, rebuild logic, add answer options css and logic

--- src/App.js
+++ src/App.js
@@ -1,34 +1,122 @@
 import React from 'react';
+import uniqid from 'uniqid';
+import he from 'he';
 import InitialView from './components/InitialView';
 import RenderQuestions from './components/RenderQuestions';
+import Button from './components/Button';
 
 function App() {
     const [quiz, setQuiz] = React.useState({
         isStarted: false,
-        questions: (() => JSON.parse(localStorage.getItem('questions')))() || getQuestions()
+        check: false,
+        answers: [],
+        questions: (() => JSON.parse(localStorage.getItem('questions')) || getQuestions())()
     });
 
     async function getQuestions() {
         const res = await fetch('https://opentdb.com/api.php?amount=10');
         const data = await res.json();
+        const newData = {};
+
+        newData['results'] = data.results.map((el) => {
+            return { ...el, id: uniqid('q-') };
+        });
         setQuiz((prevState) => ({
             ...prevState,
-            questions: data.results
+            questions: newData.results
         }));
-        localStorage.setItem('questions', JSON.stringify(data.results));
+        localStorage.setItem('questions', JSON.stringify(newData.results));
     }
-    let button = {
+
+    React.useEffect(() => {
+        async function makeQuizGameArr() {
+            if ((await quiz.questions) === undefined) {
+                return;
+            }
+            const quizGameArr = quiz.questions.map((e) => {
+                let question = he.decode(e.question);
+                let correctAnswer = he.decode(e.correct_answer);
+                let optionsArr = [];
+                let incorrectAnswers = [];
+                for (let k = 0; k < e.incorrect_answers.length; k++) {
+                    incorrectAnswers[k] = he.decode(e.incorrect_answers[k]);
+                }
+                optionsArr.push(correctAnswer, incorrectAnswers);
+                optionsArr = optionsArr.flat();
+
+                optionsArr.sort(() => (Math.random() > 0.5 ? 1 : -1));
+                return {
+                    id: e.id,
+                    question: question,
+                    correctAnswer: correctAnswer,
+                    allAnswers: optionsArr
+                };
+            });
+            setQuiz((prevState) => {
+                return { ...prevState, answers: [], quizGameArr: quizGameArr };
+            });
+        }
+        makeQuizGameArr();
+        console.log('questions changed');
+    }, [quiz.questions]);
+
+    //* handlers
+    function handleOptionClick(el, option, questionId) {
+        if (quiz.answers.find((e) => e.answer === option)) {
+            let index = quiz.answers.findIndex((e) => e.answer === option);
+            setQuiz((prevState) => {
+                let newAnswers = [...prevState.answers];
+                newAnswers.splice(index, 1);
+                return { ...prevState, answers: newAnswers };
+            });
+            return;
+        }
+
+        setQuiz((prevState) => {
+            let newAnswers = [...prevState.answers];
+            newAnswers.push({ questionId: questionId, answer: option });
+            return { ...prevState, answers: newAnswers };
+        });
+    }
+    function checkAllAnswers(answers, currentGame) {
+        currentGame.find((answer) => {
+            console.log(answer.id, '- id');
+        });
+    }
+    //* buttons
+    let buttonInitial = {
         onClick: () =>
             setQuiz((prevState) => ({
                 ...prevState,
                 isStarted: !prevState.isStarted
             })),
-        text: 'My button',
-        className: 'btn'
+        text: 'Start quiz',
+        className: 'btn btn--big '
+    };
+    let checkAnswersBtn = {
+        text: 'Check answers',
+        className: 'btn btn--normal ',
+        onClick: () => checkAllAnswers(quiz.answers, quiz.quizGameArr)
     };
     return (
         <div className="App">
-            {quiz.isStarted ? <RenderQuestions questions={quiz.questions} /> : <InitialView {...button} />}
+            {quiz.isStarted ? (
+                <div>
+                    {quiz.quizGameArr.length > 0 && (
+                        <RenderQuestions
+                            questions={quiz.quizGameArr}
+                            answers={quiz.answers}
+                            handleClick={handleOptionClick}
+                            check={quiz.check}
+                            
+                        />
+                    )}
+                    <Button {...checkAnswersBtn} />
+                    <Button onClick={getQuestions} text="Get new questions" className="btn btn--normal" />
+                </div>
+            ) : (
+                <InitialView {...buttonInitial} />
+            )}
         </div>
     );
 }
--- src/components/RenderQuestions.js
+++ src/components/RenderQuestions.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import he from 'he';
 
 export default function RenderQuestions(props) {
-    const questions = props.questions.map((e, i) => {
-        let question = he.decode(e.question);
-        let optionsArr = [];
-        let correctAnswer = he.decode(e.correct_answer);
-        let incorrectAnswers = [];
-        for (let k = 0; k < e.incorrect_answers.length; k++) {
-            incorrectAnswers[k] = he.decode(e.incorrect_answers[k]);
-        }
-        optionsArr.push(correctAnswer, incorrectAnswers);
-        optionsArr = optionsArr.flat();
+    const questions = props.questions.map((questionObj, i) => {
+        let question = questionObj.question;
 
-        optionsArr.sort(() => (Math.random() > 0.5 ? 1 : -1));
         return (
             <div key={'question-' + i} className="question--title">
                 <h4>{question}</h4>
                 <div className="question--options">
-                    {optionsArr.map((option, j) => {
+                    {questionObj.allAnswers.map((option, j) => {
+                        let classes = props.answers.find((answer) => {
+                            return answer.answer === option;
+                        })
+                            ? 'question--btn-option active'
+                            : 'question--btn-option';
+                        if (props.check) {
+                            // classes =
+                            //     answer.answer === props.correctAnswer
+                            //         ? classes + ' right'
+                            //         : classes + ' wrong';
+                        }
                         return (
-                            <div key={'option-' + j} className="question--option">
+                            <div
+                                key={questionObj.id + j}
+                                onClick={(buttonElement) =>
+                                    props.handleClick(buttonElement, option, questionObj.id)
+                                }
+                                className={classes}>
                                 {option}
                             </div>
                         );
